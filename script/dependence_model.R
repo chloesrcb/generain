18f3547abff29b7dd225ac6ae85347f893897713
Chloe Serre-Combe <chloe.serre-combe@umontpellier.fr>
2024-04-16 10:13:18 +0200

    Add arguments

---
+++ script/dependence_model.R
@@ -0,0 +1,56 @@
+library(generain)
+
+################################################################################
+# Validation of the dependence model -------------------------------------------
+################################################################################
+
+# Brown-Resnick simulation data ------------------------------------------------
+
+# true parameters
+# beta1, beta2, alpha1, alpha2
+true_param <- c(0.4, 0.2, 1.5, 1)
+
+# Simulation with 25 sites and 300 times
+list_BR_25_300 <- list()
+for (i in 1:100) {
+  file_path <- paste0("../data/simulations_BR/sim_25s_300t/rainBR_", i, ".csv")
+  df <- read.csv(file_path)
+  list_BR_25_300[[i]] <- df
+}
+
+# Simulation with 49 sites and 100 times
+list_BR_49_100 <- list()
+for (i in 1:100) {
+  file_path <- paste0("../data/simulations_BR/sim_49s_100t/rainBR_", i, ".csv")
+  df <- read.csv(file_path)
+  list_BR_49_100[[i]] <- df
+}
+
+# Evaluation of the dependence model
+
+# Simulation with 25 sites and 300 times ---------------------------------------
+simu_df <- list_BR_25_300[[1]] # first simulation
+nsites <- ncol(simu_df) # number of sites
+df_dist <- distances_regular_grid(nsites) # distance matrix
+
+# Evaluate the estimates
+spa_estim_25 <- evaluate_vario_estimates(list_BR_25_300, 0.9,
+                                      c(param[1], param[3]), spatial = TRUE,
+                                      df_dist = df_dist, hmax = sqrt(17))
+
+temp_estim_25 <- evaluate_vario_estimates(list_BR_25_300, 0.9,
+                      c(param[2], param[4]), spatial = FALSE, tmax = 10)
+
+# Simulation with 49 sites and 100 times ---------------------------------------
+
+simu_df <- list_BR_49_100[[1]] # first simulation
+nsites <- ncol(simu_df) # number of sites
+df_dist <- distances_regular_grid(nsites) # distance matrix
+
+# Evaluate the estimates
+spa_estim_49 <- evaluate_vario_estimates(list_BR_49_100, 0.8,
+                                      c(param[1], param[3]), spatial = TRUE,
+                                      df_dist = df_dist, hmax = sqrt(17))
+
+temp_estim_49 <- evaluate_vario_estimates(list_BR_49_100, 0.8,
+                      c(param[2], param[4]), spatial = FALSE, tmax = 10)
--- script/validation_optim.R
+++ script/validation_optim.R
@@ -16,97 +16,88 @@ source("../R/distances.R")
 source("../R/optimization.R")
 
 ################################################################################
-# Test each functions ----------------------------------------------------------
+# Test on exponential ----------------------------------------------------------
 ################################################################################
 
-nsites <- 25
-# npairs <- choose(nsites, 2) + nsites # or n(n+1)/2
-df_dist <- distances_regular_grid(nsites)
-npairs <- nrow(df_dist)
-
 # Simulate expo ~ chi ----------------------------------------------------------
-h_vect <- get_h_vect(df_dist, sqrt(17))
-tau_vect <- 1:10
-nconfig <- npairs * length(tau_vect)
-Tmax <- 100
-chi <- theorical_chi_mat(c(0.4, 0.2, 1.5, 1), h_vect, tau_vect)
-n_res <- 10
 
+# create a distance matrix
+nsites <- 25
+df_dist <- distances_regular_grid(nsites) # distance matrix
+npairs <- nrow(df_dist) # number of pairs
+h_vect <- get_h_vect(df_dist, sqrt(17)) # spatial lags
+tau_vect <- 1:10 # temporal lags
+nconfig <- npairs * length(tau_vect) # number of configurations
+Tmax <- 100 # number of time steps
+
+chi <- theorical_chi_mat(c(0.4, 0.2, 1.5, 1), h_vect, tau_vect) # chi matrix
+
+# simulate expo and optimize
+n_res <- 100 # number of simulations
 df_result <- evaluate_optim_simuExp(n_res, Tmax, tau_vect, h_vect, chi, df_dist,
                                     nconfig)
 
-true_param <- c(0.4, 0.2, 1.5, 1)
-
-df_valid <- get_criterion(df_result, true_param)
+true_param <- c(0.4, 0.2, 1.5, 1) # true parameters
+df_valid_exp <- get_criterion(df_result, true_param) # get RMSE, MAE, Mean
 
 
 ################################################################################
 # BR SIMU ----------------------------------------------------------------------
 ################################################################################
 
+# simu with 25 sites and 300 times ---------------------------------------------
+
 # true parameters
 # beta1, beta2, alpha1, alpha2
-param <- c(0.4, 0.2, 1.5, 1)
-
-list_BR <- list()
+true_param <- c(0.4, 0.2, 1.5, 1)
 
 # Iterate through files from 1 to 100
+list_BR <- list()
 for (i in 1:100) {
   file_path <- paste0("../data/simulations_BR/sim_25s_300t/rainBR_", i, ".csv")
   df <- read.csv(file_path)
   list_BR[[i]] <- df
 }
 
-BR_df <- list_BR[[1]]
-nsites <- ncol(BR_df)
-df_dist <- distances_regular_grid(nsites)
+BR_df <- list_BR[[1]] # first simulation
+nsites <- ncol(BR_df) # number of sites
+df_dist <- distances_regular_grid(nsites) # distance matrix
+h_vect <- get_h_vect(df_dist, sqrt(17)) # spatial lags
 
-# compute the excess ind matrix and the sum of the excesses on simulation
-data_rain <- BR_df
-
-h_vect <- get_h_vect(df_dist, sqrt(17))
-
-excesses <- empirical_excesses(BR_df, 0.9, 1:10, h_vect, df_dist)
-
-result <- optim(par = c(0.4, 0.2, 1.5, 1), fn = neg_ll, excesses = excesses,
-                h_vect = h_vect, tau = 1:10, df_dist = df_dist,
-                method = "CG")
-result$par
-
-
-df_result <- evaluate_optim(list_BR, quantile = 0.9, true_param = param,
+df_result <- evaluate_optim(list_BR, quantile = 0.9, true_param = true_param,
                             tau = 1:10, df_dist = df_dist)
 
-get_criterion(df_result, param)
-# simu 2
+df_valid_1 <- get_criterion(df_result, param)
 
-list_BR <- list()
+# simu with more sites and less time -------------------------------------------
+
+# true parameters
+# beta1, beta2, alpha1, alpha2
+true_param <- c(0.4, 0.2, 1.5, 1)
 
 # Iterate through files from 1 to 100
+list_BR <- list()
 for (i in 1:100) {
   file_path <- paste0("../data/simulations_BR/sim_49s_100t/rainBR_", i, ".csv")
   df <- read.csv(file_path)
   list_BR[[i]] <- df
 }
 
-BR_df <- list_BR[[1]]
-nsites <- ncol(BR_df)
-df_dist <- distances_regular_grid(nsites)
+BR_df <- list_BR[[1]] # first simulation
+nsites <- ncol(BR_df) # number of sites
+df_dist <- distances_regular_grid(nsites) # distance matrix
+h_vect <- get_h_vect(df_dist, sqrt(17)) # spatial lags
 
-# compute the excess ind matrix and the sum of the excesses on simulation
-data_rain <- BR_df
-
-h_vect <- get_h_vect(df_dist, sqrt(17))
-
-excesses <- empirical_excesses(BR_df, 0.8, 1:10, h_vect, df_dist)
-
+# for one simulation
+excesses <- empirical_excesses(BR_df, 0.8, 1:10, h_vect, df_dist, nmin = 10)
 result <- optim(par = c(0.4, 0.2, 1.5, 1), fn = neg_ll, excesses = excesses,
                 h_vect = h_vect, tau = 1:10, df_dist = df_dist,
                 method = "CG")
-result$par
+result$par # estimated parameters
 
-
-df_result <- evaluate_optim(list_BR, quantile = 0.9, true_param = param,
-                            tau = 1:10, df_dist = df_dist)
-
-get_criterion(df_result, param)
+# for all simulations
+df_result <- evaluate_optim(list_BR, quantile = 0.8, true_param = true_param,
+                            tau = 1:10, df_dist = df_dist, method = "CG",
+                            nmin = 10)
+# get RMSE, MAE, Mean
+df_valid_2 <- get_criterion(df_result, param)
