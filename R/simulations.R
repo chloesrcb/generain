c21f2eea25cf2c6b77de0446f820de0ba5ef58ab
Chloe Serre-Combe <chloe.serre-combe@umontpellier.fr>
2024-09-10 09:44:23 +0200

    try upd generain package

--- R/simulations.R
+++ R/simulations.R
@@ -39,116 +39,6 @@ dist_adv <- function(s1, s2, t1, t2, adv) {
   return(hnorm_adv)
 }
 
-#' sim_BR function
-#'
-#' This function performs a simulation of a spatio-temporal Brown-Resnick
-#' process using a fractionnal Brownian motion model and based on the
-#' David Leber code.
-#'
-#' @param beta1 The value of beta1.
-#' @param beta2 The value of beta2.
-#' @param alpha1 The value of alpha1.
-#' @param alpha2 The value of alpha2.
-#' @param x Vector for the first dimension (spatial x in our case).
-#' @param y Vector for the second dimension (spatial y in our case)
-#' @param t Vector for the third dimension (time in our case).
-#' @param n.BR The number of BR simulations to perform.
-#' @param adv The advection coordinates vector. Default is c(0, 0).
-#'
-#' @return The result of the simulation.
-#'
-#' @import RandomFields
-#' @import stats
-#'
-#' @export
-sim_BR <- function(beta1, beta2, alpha1, alpha2, x, y, t, n.BR, adv = c(0, 0)) {
-  # beta1, beta2, alpha1, alpha2 are variogram parameters
-  # x is the first dimension (spatial x in our case)
-  # y is the second dimension (spatial y in our case)
-  # z is the third dimension (time in our case)
-  # (adv1, adv2) advection coordinates vector
-  ## Setup
-  RandomFields::RFoptions(spConform = FALSE, install = "no")
-  lx <- length(sx <- seq_along(x))  # spatial
-  ly <- length(sy <- seq_along(y))  # spatial
-  lt <- length(st <- seq_along(t))  # temporal
-
-  ## Model-Variogram BuhlCklu (fractional Brownian motion)
-  modelBuhlCklu <- RandomFields::RMfbm(alpha = alpha1, var = beta1, proj = 1) +
-                   RandomFields::RMfbm(alpha = alpha1, var = beta1, proj = 2) +
-                   RandomFields::RMfbm(alpha = alpha2, var = beta2, proj = 3)
-
-  ## Construct grid
-  Nxy <- lx * ly # spatial grid size
-  N <- Nxy * lt # spatio-temporal grid size
-  grid <- matrix(0, nrow=N, ncol=3) # (N,3)-matrix
-
-  for (i in sx)
-    for (j in seq_len(ly*lt))
-      grid[i+(j-1)*ly, 1] <- i
-
-  for (i in sy)
-    for (j in sx)
-      for(k in st)
-        grid[j+lx*(i-1)+(k-1)*Nxy, 2] <- i
-
-  for (i in st)
-    for (j in seq_len(Nxy))
-      grid[j+Nxy*(i-1), 3] <- i
-
-  if (all(adv == c(0, 0))) {
-    grid_space <- grid[, 1:2]
-    distmat_space <- as.matrix(dist(grid_space))
-    grid_time <- grid[, 3]
-    distmat_time <- as.matrix(dist(matrix(grid_time, ncol = 1)))
-  } else {
-    distmat_space <- matrix(0, nrow=N, ncol=N)
-    distmat_time <- matrix(0, nrow=N, ncol=N)
-
-    for (i in 1:N) {
-      for (j in 1:N) {
-        s1 <- c(grid[i, 1], grid[i, 2])
-        s2 <- c(grid[j, 1], grid[j, 2])
-        t1 <- grid[i, 3]
-        t2 <- grid[j, 3]
-        distmat_space[i, j] <- dist_adv(s1, s2, t1, t2, adv)
-        distmat_time[i, j] <- abs(t1 - t2)
-      }
-    }
-  }
-
-  gamma <- gamma_theta(distmat_space, distmat_time, beta1, beta2,
-                 alpha1, alpha2)
-
-  ## Main
-  Z <- array(, dim = c(lx, ly, lt, n.BR)) # 3d array
-  E <- matrix(rexp(n.BR * N), nrow = n.BR, ncol = N)
-
-  for (i in seq_len(n.BR)) {
-    # for n = 1
-    V <- 1 / E[i, 1] # poisson process
-    W <- RandomFields::RFsimulate(modelBuhlCklu, x, y, t) # GP
-    Y <- exp(W - W[1] - gamma[,1])
-    Z[,,,i] <- V * Y
-
-    # n in {2,..,N}
-    for (n in 2:N) {
-      Exp <- E[i, n]
-      V <- 1 / Exp
-      while(V > Z[N * (i - 1) + n]) {
-        W <- RandomFields::RFsimulate(modelBuhlCklu, x, y, t)
-        Y <- exp(W - W[n] - gamma[,n])
-        if (all(V * Y[seq_len(n - 1)] < Z[(N * (i - 1) + 1):(N * (i - 1) + (n - 1))])) {
-          Z[,,, i] <- pmax(V * Y, Z[,,, i])
-        }
-        Exp <- Exp + rexp(1)
-        V <- 1 / Exp
-      }
-    }
-  }
-  ## Return
-  Z
-}
 
 #' sim_BR_adv function
 #'
@@ -172,7 +62,7 @@ sim_BR <- function(beta1, beta2, alpha1, alpha2, x, y, t, n.BR, adv = c(0, 0)) {
 #' @import stats
 #'
 #' @export
-sim_BR_adv <- function(beta1, beta2, alpha1, alpha2, x, y, z, n.BR,
+sim_BR <- function(beta1, beta2, alpha1, alpha2, x, y, z, n.BR,
                        adv) {
   ## Setup
   RandomFields::RFoptions(spConform = FALSE)
