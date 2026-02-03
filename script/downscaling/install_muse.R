4f3541839ba438e293f725a58904b5467b9b64a4
Chloe Serre-Combe <chloe.serre-combe@umontpellier.fr>
2026-02-03 09:29:52 +0100

    remove downscaling code

--- script/downscaling/install_muse.R
+++
@@ -1,7 +0,0 @@
-
-Sys.setenv(RETICULATE_PYTHON = Sys.which("python"))
-Sys.setenv(RETICULATE_LOG_LEVEL = "ERROR")
-
-stopifnot(py_module_available("tensorflow"))
-
-cat("✅ TensorFlow visible depuis R\n")
